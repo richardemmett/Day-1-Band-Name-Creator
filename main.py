62bb26811bbadfcb4893eef470350a44eee82a41
richardemmett <157606947+richardemmett@users.noreply.github.com>
2024-01-25 18:21:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+#Simple band name generator
+#Introduction to inputs, variables, and concatenation
+city = input("Enter the name of the city you were born:\n")
+pet = input("Enter the name of your first pet:\n")
+print("Your band could be called: " + city + " " + pet)
